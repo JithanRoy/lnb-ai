6949903a3561b3887833753822177fd804470752
Jithan Roy <jithanroyjony@gmail.com>
2024-06-24 13:28:25 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}', // Add this line if you have a components directory
+  ],
+  // ...rest of your configuration
+}
